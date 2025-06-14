8c19faf5a353d09a8c84d2a665e43f361cc59f51
Nienke Dekkema <nienke.dekkema@ru.nl>
2025-06-14 09:46:05 +0200

    added documentation

--- demo_conversations.py
+++ demo_conversations.py
@@ -1,59 +1,6 @@
 from naoqi import ALProxy
 import time
 
-def start_dialog(ip, port):
-    topic_paths = [
-        "/home/nao/dialogs/Introduction_enu.top",
-        "/home/nao/dialogs/Entrance_enu.top",
-        "/home/nao/dialogs/Middlefloor_enu.top",
-        "/home/nao/dialogs/Elevators_enu.top"
-    ]
-
-    try:
-        dialog = ALProxy("ALDialog", ip, port)
-        tts = ALProxy("ALTextToSpeech", ip, port)
-        memory = ALProxy("ALMemory", ip, port)
-
-        dialog.setLanguage("English")
-        tts.say("Starting dialog sequence.")
-        topics = dialog.getLoadedTopics()
-        print("topics")
-        print(topics)
-
-        
-        for i, path in enumerate(topic_paths):
-            subscription_name = "dialog_sequence_{}".format(i)
-            topic_name = dialog.loadTopic(path)
-            dialog.activateTopic(topic_name)
-            dialog.subscribe(subscription_name)
-            dialog.forceInput("onStart")
-
-            # Reset memory signal before dialog
-            memory.insertData("my_dialog/finished", 0)
-
-            # Wait until dialog writes to memory (with timeout)
-            max_wait = 40  # seconds
-            waited = 0
-            while memory.getData("my_dialog/finished") != 1 and waited < max_wait:
-                time.sleep(0.5)
-                waited += 0.5
-
-            if waited >= max_wait:
-                tts.say("Timeout waiting for dialog to finish.")
-
-            # Clean up
-            dialog.unsubscribe(subscription_name)
-            dialog.deactivateTopic(topic_name)
-            dialog.unloadTopic(topic_name)
-
-        tts.say("All dialogs completed.")
-
-    except Exception as e:
-        print("Error running dialogs:")
-        print(e)
-
-
-
 
 def have_one_dialog(ip, port, landmark):
     topic_paths = [
@@ -83,7 +30,7 @@ def have_one_dialog(ip, port, landmark):
         # dialog.unsubscribe("dialog_sequence_{}".format(1))
         dialog.deactivateTopic(topic_name_E)
         dialog.unloadTopic(topic_name_E)
-        if landmark == 0:
+        if landmark == 0: # set the correct conversation corresponding with the landmark it is standing infront of.
             print("introduction")
             i = 0
                 
--- demo_pepper.py
+++ demo_pepper.py
@@ -5,139 +5,102 @@ import qi
 from naoqi import ALProxy
 import demo_movements as movements
 import demo_conversations as conversations
-# import beckon
-# import movement
+import math
 
 
 # ip="127.0.0.1" #Virtual Robot ip
 ip="192.168.0.119" #Actual Robot ip
 port = 9559
 
-# Voor ons: eerste keer 1x wake up en autonomousoff
 
-
+# Initalize all the proxies we need
 motion = ALProxy("ALMotion", ip, port)
 life = ALProxy("ALAutonomousLife", ip, port)
 navigation = ALProxy("ALNavigation", ip, port)
-# posture = ALProxy("ALRobotPosture", ip, port)
-# posture = ALProxy("ALRobotPostureProxy", ip, port)
-life.setState("disabled")
-# 
+life.setState("disabled") # we turn autonomous mode off
 memoryProxy = ALProxy("ALMemory", ip, 9559)
 landmarkProxy = ALProxy("ALLandMarkDetection", ip, 9559)
 landmarkProxy.subscribe("landmarkTest")
 leds = ALProxy("ALLeds", ip, port)
 eye_leds = "FaceLeds"
-# life = ALProxy("ALAutonomousLife", ip, port)
-# life.setState("disabled")
-# def findTarget():
-#     motion.setAngles("HeadYaw", -1, 0.2) # look left
-#     detected = False
-#     navigation.navigateTo(0.5, 0)
-#     while not detected:
-#         markData = memoryProxy.getData("LandmarkDetected")
-#         nao.Say("I am starting to detect")
-#         while (len(markData) == 0):
-#             markData = memoryProxy.getData("LandMarkDetected")
-#         print(markData)
-#         detected = True
-#         # leds.fadeRGB("FaceLeds", 0x0000FF, 0.1)
-#         # detected, _, landmarkinfo = nao.DetectLandMark()
-#         print(detected)
-#         # time.sleep(2)
-#         # print(detected)
-#         # leds.fadeRGB("FaceLeds", 0x000000, 0.5)
-#         # navigation.navigateTo(0.5, 0) # move one meter forwards
-#         # print(detected)
-#         # time.sleep(1)
-#     nao.Say("I have detected the landmark")
-#     # print(landmarkinfo)
-#     # # nao.Say("detected")
-#     # # print(landmarkinfo[0][0])
-#     # landmarkNumber = landmarkinfo[0][0]
-#     # return landmarkNumber
-#     return 5
 
-import math
+
 def findTarget():
-    detected = False
-    motion.moveTo(0, 0, math.pi / 2)
+    """
+    Let Pepper locate the next target. It does this by turning 90 degrees left, turning its head also 90 degrees left,
+    moving 30 centimeters forward, and looking for a target. If it is not found, it rotates its body another 90 degrees left,
+    such that it is facing the wall. It moves its head 90 degrees right such that it looks forward. If it is not detected, it
+    turns 90 degrees right, and its head 90 degrees left, and it once again moves 30 centimeters forward. This process is
+    repeated untill the target is found. Once located, Pepper moves such that it is facing which its back towards the target,
+    such that it is looking at the crowd.
+
+    Returns:
+        int: the number of the landmark it has detected.
+    """
+    detected = False # not detected
+    motion.moveTo(0, 0, math.pi / 2) # rotate 90 degrees left
     motion.setAngles("HeadYaw", 1.2, 0.2) # look left
-    # detected = False
-    # nao.Say("start while loop one")
     time.sleep(1)
     while not detected:
-        direction = "correct"
-        navigation.navigateTo(0.3, 0)
+        direction = "correct" # standing parallel with the wall
+        navigation.navigateTo(0.3, 0) # move 30 centimeters forward
         time.sleep(2)
-        detected, _, landmarkinfo = nao.DetectLandMark()
-        print(detected)
-        if detected:
-            # nao.Say("detected")
+        detected, _, landmarkinfo = nao.DetectLandMark() # try to detect the landmark
+        if detected: # the landmark has been found, so we discontinue the loop
             break
-        motion.setAngles("HeadYaw", 0, 0.2)
-        motion.moveTo(0, 0, math.pi/2)
+        # the landmark is not detected
+        motion.setAngles("HeadYaw", 0, 0.2) # look forward
+        motion.moveTo(0, 0, math.pi/2) # standing facing the wall
         direction = "wall"
-        if detected:
-            # nao.Say("detected")
+        if detected:# landmark has been found, discontinue the loop
             break
-        motion.setAngles("HeadYaw", 1.2, 0.2)
-        motion.moveTo(0, 0,  - math.pi/2)
-        direction = "correct"
+        motion.setAngles("HeadYaw", 1.2, 0.2) # look left
+        motion.moveTo(0, 0,  - math.pi/2) # move right such that pepper is parallel with the wall
+        direction = "correct" # standing parallel with the wall
 
 
-        # distance to target
-        #calculate distance target
-    # nao.Say("exited while loop one")
-    landmarkTheoreticalSize = 0.092
+    # get the proxies we need
     memoryProxy = ALProxy("ALMemory", ip, 9559)
     landmarkProxy = ALProxy("ALLandMarkDetection", ip, 9559)
     landmarkProxy.subscribe("landmarkTest")
     markData = memoryProxy.getData("LandmarkDetected")
     
-    while (len(markData) == 0):
-        markData = memoryProxy.getData("LandMarkDetected")
-    # nao.Say("detected")
-    print(landmarkinfo)
-    print(landmarkinfo[0][0])
-    if direction == "wall":
-        motion.setAngles("HeadYaw", 0, 0.2)
-        motion.moveTo(0, 0, - math.pi)
-    else: # correct
-        motion.setAngles("HeadYaw", 0, 0.2)
-        motion.moveTo(0, 0, - math.pi / 2)
+    while (len(markData) == 0): # the landmark is not detected
+        markData = memoryProxy.getData("LandMarkDetected") # detect the landmark
+    print(landmarkinfo[0][0]) # we can see whether it detects the correct landmark
+    if direction == "wall": # faced towards wall, has to rotate 180 degrees
+        motion.setAngles("HeadYaw", 0, 0.2) # look forward
+        motion.moveTo(0, 0, - math.pi) # rotate body 180 degrees
+    else: # positioned parallel with wall, has to rotate 90 degrees right
+        motion.setAngles("HeadYaw", 0, 0.2) # look forward
+        motion.moveTo(0, 0, - math.pi / 2) # rotate body 90 degrees to the right
     
-    return landmarkinfo[0][0]
+    return landmarkinfo[0][0] # return the landmarknumber
 
 
 if __name__ == "__main__":
-    nao.InitProxy(ip,[0],port)
-    motion.wakeUp()
-    #nao.Say("Come join, the tour is about to start!")
-    # movements.wave()
-    # conversations.start_dialog(ip, port)
-     # start implementeren
+    nao.InitProxy(ip,[0],port) # connect to Pepper
+    motion.wakeUp() # make Pepper "alive" with autonomous mode turned off
     time.sleep(1)
-
-    # introduction tekst
-    # look for target and tell information
     conversations_had = 0
-    landmarkNumber = 0
-    while True:
-        if conversations_had == 0:
-            movements.wave(ip, port)
-        elif landmarkNumber == 64 or landmarkNumber == 80:
+    landmarkNumber = 0 # initial value, starting point of tour, no landmark is detected yet so we set this to 0
+    while True: # endless loop
+        # first have the conversation matching the landmark
+        if conversations_had == 0: 
+            movements.wave(ip, port) # gather attention, welcome people by waving
+        elif landmarkNumber == 64 or landmarkNumber == 80: # want people to know pepper wants to say someting
             nao.Say("Gather around.")
             movements.gather_around(ip, port)
-        elif landmarkNumber == 85:
+        elif landmarkNumber == 85: # Pepper talks about something it just passed, so it points to it
             movements.point_to(ip, port)
-        else:
+        else: # Pepper does not have any text for this landmark, so it covers its eyes
              movements.hide_eyes(ip, port)
-        # conversations.have_one_dialog(ip, port, landmarkNumber)
-        nao.Tracker()
-        if landmarkNumber == 0:
+        # conversations.have_one_dialog(ip, port, landmarkNumber) # normally we do this and in this function it selects the
+        # corresponding conversation, however, the implementation does not work so we have implemented something else that
+        # shows the conversations and what is said in there
+        nao.Tracker() # track faces while pepper is talking
+        if landmarkNumber == 0: # first conversation
             nao.Say("Here I welcome you to my tour. This is the first conversation.")
-                
         elif landmarkNumber == 64:
                 nao.Say("Here you can find the restaurant Brownies and Downies. This is the second conversation. I make a joke that I hope I don't fall of the stairs.")
         elif landmarkNumber == 80:
@@ -147,22 +110,22 @@ if __name__ == "__main__":
         else:
              nao.Say("Unfortunately I do not have any information about this location.")
         time.sleep(3)
-        nao.Tracker(0)
+        nao.Tracker(0) # turn face tracker off such that it can focus on finding the next landmark
         print(landmarkNumber)
         conversations_had += 1
-        if landmarkNumber == 64:
+        if landmarkNumber == 64: # nao makes a joke, and it makes a corresponding movement
              movements.hide_eyes(ip, port)
              time.sleep(1)
-        if conversations_had == 4:
+        if conversations_had == 4: # all conversations have been had
             time.sleep(2)
             nao.Say("Now it is time to give me a big round of applause.")
             movements.bow(ip, port)
             time.sleep(5)
-            break
+            break # quit the loop
         time.sleep(2)
         movements.join_turn
         time.sleep(1)
-        landmarkNumber = findTarget()
+        landmarkNumber = findTarget() # move to the next target
         time.sleep(2)
         print("landmark detected", landmarkNumber)
         
